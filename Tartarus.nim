4f00b59c8f4c5eba2f75e0fbba6c908e6e454c60
Buerer <81548543+Mosstone@users.noreply.github.com>
2025-05-26 22:53:15 +0200

    Add files via upload

---
+++ Tartarus.nim
@@ -0,0 +1,20 @@
+import
+    # tables,
+    # sets,
+    # httpclient,
+    # posix,
+    # os,
+    osproc
+
+
+
+
+
+
+
+proc main() =
+  echo "test"
+  echo execProcess("echo alsotest")
+
+when isMainModule:
+  main()
